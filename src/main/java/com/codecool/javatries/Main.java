74b4dd2b6c12a76fa97eda4bb8005e84c2e3ee3b
Ritika jain <ritikajain49026@gmail.com>
2020-05-11 14:53:35 +0530

    new feature added

--- src/main/java/com/codecool/javatries/Main.java
+++ src/main/java/com/codecool/javatries/Main.java
@@ -1,7 +1,11 @@
 package com.codecool.javatries;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
@@ -9,7 +13,6 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-
         Path worldListPath = new File("assets/wordlist.txt").toPath();
         List<String> wordList = Files.readAllLines(worldListPath);
 
@@ -18,10 +21,40 @@ public class Main {
         for (String word : wordList) {
             ac.addWord(word);
         }
-        List<String> results = ac.autoComplete("heaven");
+        boolean flag=false;
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        String query=br.readLine();
+        List<String> results = ac.autoComplete(query);
+        if(results.size()!=0)
+        	flag=true;
         for(String str:results) {
         	System.out.println(str);
         }
-        System.out.println("Results Done");
+        if(flag==true) {
+        	System.out.println("Results Done");
+        }
+        else {
+        	char cc;
+        	do {
+            	System.out.print("Do you want to add this word to list(Y/N)?");
+            	cc=br.readLine().charAt(0);
+            	if(cc=='Y'||cc=='y') {
+            		BufferedWriter out = new BufferedWriter( 
+                            new FileWriter("assets/wordlist.txt",true));
+            		  out.write("\r\n");
+            		  out.write(query); 
+            		  out.close();
+            		  System.out.println("Word Added to WordList");
+            		  return;
+            	}else if(cc=='N'||cc=='n') {
+            		return;
+            	}else {
+            		System.out.println("Incorrect Input");
+            	}
+        	}while(true);
+        	
+        }
+         
+		  
     }
 }
